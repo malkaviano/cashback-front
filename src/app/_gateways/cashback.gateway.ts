74eacf4dd9edb63dcef1ed511ff5dce7a1e024ff
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-02-28 15:04:43 -0300

    gateway tests

--- src/app/_gateways/cashback.gateway.ts
+++ src/app/_gateways/cashback.gateway.ts
@@ -10,6 +10,7 @@ export class CashbackGateway {
   constructor(private http: HttpClient, @Inject('CASHBACK_URL') private url: string) { }
 
   get(): Observable<any> {
+    // TODO: Remove this string, server should check the logged CPF
     return this.http.get(`${this.url}/123123123`);
   }
 }
--- src/app/_gateways/cashback.gatway.spec.ts
+++ src/app/_gateways/cashback.gatway.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CashbackGateway } from './cashback.gateway';
 
 describe('Cashbackgateway', () => {
   let gateway: CashbackGateway;
+  let httpMock: HttpTestingController;
+  let url = 'http://testeUrl';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: 'CASHBACK_URL', useValue: url }
+      ]
+    });
+
     gateway = TestBed.inject(CashbackGateway);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(gateway).toBeTruthy();
+  });
+
+  describe("#get", () => {
+    it("gets the cashback response", () => {
+      gateway.get().subscribe();
+
+      const req = httpMock.expectOne("http://testeUrl/123123123");
+
+      expect(req.request.method).toBe("GET");
+    });
   });
 });
