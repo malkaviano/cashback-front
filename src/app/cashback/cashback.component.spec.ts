ac8b5ca9c6b9a86c0477e9b6df7e3fe888beedbd
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-02-28 17:46:30 -0300

    cashback tests

--- src/app/cashback/cashback.component.spec.ts
+++ src/app/cashback/cashback.component.spec.ts
@@ -1,25 +1,41 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { CashbackComponent } from './cashback.component';
+import { mock, instance, when } from 'ts-mockito';
+import { MockComponent } from 'ng-mocks';
+import { of } from 'rxjs';
 
-// describe('CashbackComponent', () => {
-//   let component: CashbackComponent;
-//   let fixture: ComponentFixture<CashbackComponent>;
+import { CashbackComponent } from './cashback.component';
+import { CashbackGateway } from '../_gateways/cashback.gateway';
+import { TitleComponent } from '../title/title.component';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CashbackComponent ]
-//     })
-//     .compileComponents();
-//   }));
+const mockedGateway = mock(CashbackGateway);
+const gateway = instance(mockedGateway);
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CashbackComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+when(mockedGateway.get()).thenReturn(of({ body: { credit: 10 } }));
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('CashbackComponent', () => {
+    let component: CashbackComponent;
+    let fixture: ComponentFixture<CashbackComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                CashbackComponent,
+                MockComponent(TitleComponent)
+            ],
+            providers: [
+                { provide: CashbackGateway, useValue: gateway }
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CashbackComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component.value).toBe(10);
+    });
+});
