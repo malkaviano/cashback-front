3dd2cbdbaef6387e702437dd079e21afb8b6e866
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-03-02 13:54:52 -0300

    home component and e2e

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MockComponent } from 'ng-mocks';
+
 import { HomeComponent } from './home.component';
+import { TitleComponent } from '../title/title.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +11,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        MockComponent(TitleComponent)
+      ]
     })
     .compileComponents();
   }));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public title: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.title = 'HOME';
   }
 
 }
